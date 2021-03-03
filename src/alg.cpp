c5ac4271da8f6a18e18405dd3ed93a366e8855ec
RomDmitriy <55810251+RomDmitriy@users.noreply.github.com>
2021-03-03 13:51:48 +0300

    Ивашкин Алексей

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,15 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+ if (a != b) {
+  if (a > b) {
+   a = a - b;
+  }
+  else {
+   b = b - a;
+  }
+ }
+ else {
+  return a;
+ }
 }
